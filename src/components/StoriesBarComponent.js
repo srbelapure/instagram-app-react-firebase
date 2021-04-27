f58a460722d25882eb8bfafe201ba33a717aacf1
sonali-code-base <sonalipatwardhan2017@gmail.com>
2021-04-27 18:41:27 +0530

    Added progress bar for stories uploading - APP is working

--- src/components/StoriesBarComponent.js
+++ src/components/StoriesBarComponent.js
@@ -1,13 +1,13 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { Avatar } from "@material-ui/core";
 import { db } from "./Firebase";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import firebase from "firebase";
 
 function StoriesBarComponent(props) {
-  
   const [imageUrl, setImageUrl] = useState([]);
-  let downloadFilesUrlFromFireStore=[]
+  const [progress, setProgress] = useState(0);
+  let downloadFilesUrlFromFireStore = [];
 
   const handleFileChange = (e) => {
     for (let i = 0; i < e.target.files.length; i++) {
@@ -17,7 +17,6 @@ function StoriesBarComponent(props) {
       setImageUrl((prevState) => [...prevState, newFile]);
     }
   };
-
 
   const addUserStory = async () => {
     const promises = [];
@@ -30,11 +29,14 @@ function StoriesBarComponent(props) {
       promises.push(uploadTask);
       uploadTask.on(
         firebase.storage.TaskEvent.STATE_CHANGED,
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        (snapshot) =>
+        {
+          //on state change of the file upload progress, get a snapshot continuously and update the progress function
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          ); // percentage for progress bar
           if (snapshot.state === firebase.storage.TaskState.RUNNING) {
-            console.log(`Progress: ${progress}%`);
+            setProgress(progress); // set progress from 0 to 100
           }
         },
         (error) => console.log(error.code),
@@ -43,7 +45,8 @@ function StoriesBarComponent(props) {
           downloadFilesUrlFromFireStore.push(downloadURL);
 
           if (downloadFilesUrlFromFireStore.length === imageUrl.length) {
-            alert("task upload done for all original elements");
+            alert("All the selected files have been uploaded..."+`\n`+ "you can now see the stories");
+            setProgress(0) // after successful loading, reset to fresh state
             db.collection("stories").add({
               timestamp: firebase.firestore.FieldValue.serverTimestamp(), // this gets server timestamp, hence it remains unified
               imageurl: downloadFilesUrlFromFireStore, // url => has the value of uploaded image
@@ -54,19 +57,41 @@ function StoriesBarComponent(props) {
       );
     });
     Promise.all(promises)
-      .then(() => alert("All files uploaded"))
+      .then(() => promises.length > 0?
+      alert("All files uploaded...") : alert('No files selected...'+ `\n` + 'Fist select the files form choose files option')
+      )
       .catch((err) => console.log(err.code));
   };
 
   return (
     <div className="stories-bar-container">
-      {/* <progress className="image-upload-progress" max="100" value={progress} /> */}
-      <div>
+      {
+      console.log("progressprogress",progress)
+      }
+      {progress === 0 ? (
+        <progress
+          className="stories-upload-progress"
+          max="100"
+          value={progress}
+          style={{display:'none'}}
+        />
+      ) : (
+        <progress
+          className="stories-upload-progress"
+          max="100"
+          value={progress}
+        />
+      )}
+      <div className="select-user-stories-to-add">
         <input type="file" onChange={handleFileChange} multiple />
+        <span
+          title="click on this button to upload stories selected...."
+          className="add-new-story-for-user"
+          onClick={addUserStory}
+        >
+          <i className="fas fa-plus-circle add-user-story"></i>
+        </span>
       </div>
-      <span className="add-new-story-for-user" onClick={addUserStory}>
-        <i className="fas fa-plus-circle add-user-story"></i>
-      </span>
 
       {props.stories.map((story) => {
         return (
@@ -83,6 +108,3 @@ function StoriesBarComponent(props) {
 }
 
 export default StoriesBarComponent;
-
-
-
